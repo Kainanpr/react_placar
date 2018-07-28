6d2d20293dbe9271c0c51a5db06bfb098ebeabeb
kainanpr <kainanpr95@gmail.com>
2018-07-28 14:45:52 -0300

    Correcao componente Button

--- src/componentes/Button.js
+++ src/componentes/Button.js
@@ -1,15 +1,6 @@
 import React from 'react';
 
 export class Button extends React.Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-    handleClick(event) {
-        
-    }
-
     render() {
         return (
             <button onClick={this.props.onClick}>Gol!</button>
